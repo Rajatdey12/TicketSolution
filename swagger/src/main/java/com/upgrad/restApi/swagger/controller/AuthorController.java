8b8d2db45591e65b7111774ddff9f1c69c91db6f
Rajat Dey <giet13me131@gmail.com>
2021-03-17 11:35:23 +0530

    changes

--- swagger/src/main/java/com/upgrad/restApi/swagger/controller/AuthorController.java
+++ swagger/src/main/java/com/upgrad/restApi/swagger/controller/AuthorController.java
@@ -6,6 +6,7 @@ import com.upgrad.restApi.swagger.service.AuthorService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class AuthorController {
     @Autowired
     private AuthorService service;
 
-    @RequestMapping("/getAllAuthors")
+    @GetMapping("/getAllAuthors")
     @ApiOperation(value = "Get Authors", notes = "Get all the Authors")
     public ArrayList<AuthorDto> getAllAuthors() {
         return service.getAuthors();
